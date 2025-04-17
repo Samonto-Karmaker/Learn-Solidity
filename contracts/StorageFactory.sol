3c315ebb6f5a80683380724ddcbd11517c6261ef
Samonto-Karmaker <samonto0726@gmail.com>
2025-04-17 14:05:18 +0600

    Interact with a contract inside another contract

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -10,4 +10,12 @@ contract StorageFactory {
         SimpleStorage simpleStorage = new SimpleStorage();
         storages.push(simpleStorage);
     }
+
+    function sfStore(uint256 _idx, uint256 _num) public {
+        storages[_idx].store(_num);
+    }
+
+    function sfRetrieve(uint256 _idx) public view returns (uint256) {
+        return storages[_idx].retrieve();
+    }
 }
