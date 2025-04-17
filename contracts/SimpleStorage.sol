b18198b97b7db2abfa9167b09051388f88d45a17
Samonto-Karmaker <samonto0726@gmail.com>
2025-04-17 12:14:52 +0600

    Basics of Solidity: struct & array

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,7 +2,23 @@
 pragma solidity ^0.8.8; // Sets the minimum Solidity version to use in the contract
 
 contract SimpleStorage {
-    uint256 public num;
+    uint256 num;
+
+    // struct -> JS objects
+    struct People {
+        uint256 age;
+        string name;
+    }
+    People public people1 = People({
+        age: 45,
+        name: "Luis"
+    });
+
+    // Dynamic public array
+    People[] public people;
+
+    // Static array with max length 3
+    // uint[3] public scores;
 
     function store(uint256 _num) public {
         num = _num;
@@ -12,5 +28,13 @@ contract SimpleStorage {
     // but if called inside a transaction (on-chain), the overall transaction will cost gas.
     function retrieve() public view returns (uint256) {
         return num;
+    }
+
+    // memory -> used for complex data types; 
+    // stores the value inside a temporary memory location
+    // wipes out after the function completes execution
+    // costs less gas than "storage"
+    function addPeople(uint _age, string memory _name) public {
+        people.push(People(_age, _name));
     }
 }
