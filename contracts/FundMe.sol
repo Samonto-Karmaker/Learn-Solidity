cb9097ffc1b74dbfb75c6dc5de2a0d38fd5512b5
Samonto-Karmaker <samonto0726@gmail.com>
2025-04-23 14:17:48 +0600

    Update fund to use usd and keep record of funds

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,9 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "./PriceConverter.sol";
+
 contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_FUND_USD = 1 * 1e18;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
     function fund() public payable {
-        require(msg.value >= 0.0001 ether, "too little amount");
+        require(msg.value.getConversionRate() >= MINIMUM_FUND_USD, "Amount too low");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
 
     // function withdraw() {}
