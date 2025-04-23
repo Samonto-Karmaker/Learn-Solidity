1cbd24b68b7bf48fa331efcae0e4f056da1adc64
Samonto-Karmaker <samonto0726@gmail.com>
2025-04-23 11:23:34 +0600

    Add min require fund check

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.8;
 
 contract FundMe {
-    
+    function fund() public payable {
+        require(msg.value >= 0.0001 ether, "too little amount");
+    }
+
+    // function withdraw() {}
 }
